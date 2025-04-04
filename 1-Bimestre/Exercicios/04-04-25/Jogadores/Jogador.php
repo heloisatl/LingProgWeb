74ee68774f62f5d54336fa580927579d5f895b9f
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2025-04-04 15:32:07 -0300

    Update Jogador.php

--- 1-Bimestre/Exercicios/04-04-25/Jogadores/Jogador.php
+++ 1-Bimestre/Exercicios/04-04-25/Jogadores/Jogador.php
@@ -1,40 +1,37 @@
 <?php
 
-function desenhaLinha($numero, $nome, $corFundo){
-    
-    $cor = ($corFundo == 'amarelo') ? 'yellow' : 'green';
-
-
-    echo "<tr> style='background-color: $cor;'>";
-    echo "<td>$numero </td>";
-    echo "<td>$nome </td>";
+function desenhaLinha($numero, $nome, $cor)
+{
+    echo "<tr>";
+    echo "<td style='background-color: $cor'>$numero</td>";
+    echo "<td style='background-color: $cor'>$nome</td>";
     echo "</tr>";
+}
 
-    $jogadores = [
-        "1", "Tafarel" , "amarelo",
-        "13", "Jorginho" , "verde",
-        "15", "Aldair", "amarelo",
-        "6", "marcio Santos", "verde",
-        "5", "TMauro Silva" , "amarelo",
-        "8", "Dunga" , "verde",
-        "17", "Mazinho", "amarelo",
-        "9", "Zinho", "verde",
-        "11", "Romário", "amarelo",
-        "7", "Bebeto", "verde"
-        
-    ];
+echo "<h1 style='font-size:20px'> Escalação da seleção brasileira de futebol campeã mundial em 1994:  </h1>";
+
+echo "<table border='1' style='background-color: white'>";
+
 echo "<tr>";
 echo "<th>Número</th>";
 echo "<th>Nome</th>";
 echo "</tr>";
 
-foreach ($jogadores as $jogador) {
-    desenhaLinha($jogador[0], $jogador[1], $jogador[2], $jogador[3], $jogador[2]);
-}
 
+desenhaLinha(1, "Tafarel", "yellow");
+desenhaLinha(2, "Jorginho", "green");
+desenhaLinha(13, "Aldair", "yellow");
+desenhaLinha(15, "Márcio Santos", "green");
+desenhaLinha(6, "Branco", "yellow");
+desenhaLinha(5, "Mauro Silva", "green");
+desenhaLinha(8, "Dunga", "yellow");
+desenhaLinha(17, "Malzinho", "green");
+desenhaLinha(9, "Zinho", "yellow");
+desenhaLinha(11, "Romário", "green");
+desenhaLinha(7, "Bebeto", "yellow");
 echo "</table>";
 
-}
+
 
 
 
