6dd723e0d5cf3cfdfee4f472845052611d2604ba
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2025-04-04 13:38:56 -0300

    Update Pokemon.php

--- 1-Bimestre/Exercicios/04-04-25/Pokemons/model/Pokemon.php
+++ 1-Bimestre/Exercicios/04-04-25/Pokemons/model/Pokemon.php
@@ -1,1 +1,64 @@
+<?php
 
+class Pokemon {
+
+    private string $imagem;
+    private string $nome;
+    private string $tipo;
+    private string $link;
+
+    public function __construct($imagem, $nome, $tipo, $link) {
+        $this->imagem = $imagem;
+        $this->nome = $nome;
+        $this->tipo = $tipo;
+        $this->link = $link;
+    }
+
+    public function getImagem(): string
+    {
+        return $this->imagem;
+    }
+
+    public function setImagem(string $imagem): self
+    {
+        $this->imagem = $imagem;
+
+        return $this;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getTipo(): string
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
