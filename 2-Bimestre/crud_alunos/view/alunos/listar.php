962480dde179dca0383cd875035c0180ed9d64de
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2025-07-04 15:46:12 -0300

    Add files via upload

---
+++ 2-Bimestre/crud_alunos/view/alunos/listar.php
@@ -0,0 +1,12 @@
+<?php
+    //Incluir o header
+    include_once(__DIR__."/../include/header.php");
+?>
+
+Listagem de alunos
+
+<?php
+//Incluir o footer
+    include_once(__DIR__."/../include/footer.php");
+
+?>
